cbdccdf8d33a2ea5e44e8ce5c4b7fb2c8323bc94
rokssoliana <roksasm@gmail.com>
2026-09-19 14:13:06 +0300

    lab02: add Core classlib, environment report, publish comparison

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,17 +1,15 @@
-﻿using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Text;
+using Core;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-Console.WriteLine("CrossApp - практикум з крос-платформного програмування");
-Console.WriteLine("Студентка: Смішко Роксоляна, група ФЕІ-37");
+EnvironmentReport report = EnvironmentInfo.Collect();
+
+Console.WriteLine("CrossApp - інформація про середовище");
 Console.WriteLine(new string('-', 52));
-Console.WriteLine($"OC (OSDescription) : {RuntimeInformation.OSDescription}");
-Console.WriteLine($"OC (Environment)    : {Environment.OSVersion}");
-Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
-Console.WriteLine($"Версія .NET (CLR)   : {Environment.Version}");
-Console.WriteLine($"Runtime             : {RuntimeInformation.FrameworkDescription}");
-Console.WriteLine($"Каталог застосунку  : {AppContext.BaseDirectory}");
-Console.WriteLine($"Поточний каталог    : {Environment.CurrentDirectory}");
-Console.WriteLine(new string('-', 52));
-Console.WriteLine("Предметна область   : Склад (товари, партії, залишки, переміщення)");
+Console.WriteLine($"OC              : {report.OsDescription}");
+Console.WriteLine($"Runtime         : {report.FrameworkDescription}");
+Console.WriteLine($"Архітектура     : {report.ProcessArchitecture}");
+Console.WriteLine($"RID (визначено) : {report.DetectedRid}");
+Console.WriteLine($"RID (від .NET)  : {report.ReportedRid}");
+Console.WriteLine($"Каталог         : {report.BaseDirectory}");
